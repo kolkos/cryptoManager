bbe0da7fe69aea32ab8275897b5ba21985a8afff
Anton <a.vanderkolk@kolkos.nl>
2018-01-07 11:42:56 +0100

    register advanced transactions

--- src/main/java/nl/kolkos/cryptoManager/controllers/TransactionController.java
+++ src/main/java/nl/kolkos/cryptoManager/controllers/TransactionController.java
@@ -91,6 +91,74 @@ public class TransactionController {
 		return redirect;
 	}
 	
+	@GetMapping("/advanced")
+    public String depositFormAdvanced(Model model) {
+		model.addAttribute("transaction", new Transaction());
+		model.addAttribute("transactionTypes", transactionTypeRepository.findAll());
+        model.addAttribute("walletList", walletService.findByPortfolioUsersEmail(userService.findLoggedInUsername()));
+        
+        return "transaction_form_advanced";
+    }
+	
+	@PostMapping(path="/advanced") // Map ONLY POST Requests
+	public String addAdvancedWithdrawal (
+			@RequestParam Date transactionDate,
+			@RequestParam long fromWallet,
+			@RequestParam double amountSold,
+			@RequestParam long toWallet,
+			@RequestParam double amountBought,
+			@RequestParam double transactionValue,
+			@RequestParam String remarks,
+			@RequestParam(value="addAnotherTransaction", required=false) boolean addAnotherTransaction,
+			Model model) {
+		
+		
+		TransactionType withdrawal = transactionTypeRepository.findByType("Withdrawal");
+		TransactionType deposit = transactionTypeRepository.findByType("Deposit");
+		
+		// check if the user is authorized to the wallets
+		boolean accessFromWallet = userService.checkIfCurrentUserIsAuthorizedToWallet(fromWallet);
+		boolean accessToWallet = userService.checkIfCurrentUserIsAuthorizedToWallet(fromWallet);
+		if(!accessFromWallet || !accessToWallet) {
+			User user = userService.findUserByEmail(userService.findLoggedInUsername());
+			model.addAttribute("authorizationError", true);
+			model.addAttribute("firstName", user.getName());
+			model.addAttribute("object", "wallet");
+			return "error_page";
+		}
+		
+		// now get the wallets
+		Wallet fromWalletObject = walletService.findById(fromWallet);
+		Wallet toWalletObject = walletService.findById(toWallet);
+		
+		// check if both wallets exists
+		if(fromWalletObject == null || toWalletObject == null) {
+			model.addAttribute("notFoundError", true);
+			model.addAttribute("object", "wallet");
+			return "error_page";
+		}
+		
+		// append the from and to coin to the transaction
+		String fromCoinSymbol = fromWalletObject.getCoin().getCoinMarketCapCoin().getSymbol();
+		String toCoinSymbol = toWalletObject.getCoin().getCoinMarketCapCoin().getSymbol();
+		remarks = String.format("%s -> %s (%s)", fromCoinSymbol, toCoinSymbol, remarks);
+		
+		// now create and save the transactions
+		// the withdrawal
+		transactionService.createTransaction(transactionDate, amountSold, transactionValue, fromWalletObject, remarks, withdrawal);
+		// the deposit
+		transactionService.createTransaction(transactionDate, amountBought, transactionValue, toWalletObject, remarks, deposit);
+		
+		String redirect;
+		if(addAnotherTransaction) {
+			redirect = "redirect:/transaction/advanced";
+		}else {
+			redirect = "redirect:/transaction/results";
+		}
+		
+		return redirect;
+	}
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/results")
 	public String showResults(Model model, 
 			@RequestParam(name = "search", required=false) String search,
